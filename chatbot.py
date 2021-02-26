545f042d3016bc71dbc86cc4d188bd79fa7902d2
NimaAB <nimabewrani98@gmail.com>
2021-02-26 20:28:45 +0100

    now a simple verson of the chatbot working, next step is to implement the client.py and server.py

--- chatbot.py
+++ chatbot.py
@@ -12,7 +12,7 @@ from person import *
 
 #Global variables
 ACTIONS = ["drink","eat","play","fight","steal","code","write","sleep","swim","read"]
-PERSONS = ["Alice", "Bob", "Dore", "Chuck"]
+PERSONS = ["Alice", "Bob", "Dora", "Chuck"]
 
 def peak_person(person=None):
     if (person == None) or (person not in PERSONS):
@@ -21,8 +21,24 @@ def peak_person(person=None):
     else:
         return person
 
-def peak_method(person=None):
+def peak_bot(person=None):
     person = peak_person(person)
-    switch():
+    action = random.choice(ACTIONS)
+    switch = {
+            "Alice": alice(action),
+            "Bob": bob(action),
+            "Dora": dora(action),
+            "Chuck": chuck(action)
+            }
+    return switch[person]
+
+
+if len(sys.argv) == 1:
+    msg = peak_bot(None)
+else:
+    msg = peak_bot(sys.argv[1])
+
+print(msg)
+
 
 
--- person.py
+++ person.py
@@ -2,16 +2,16 @@
 
 #First person:
 def alice(action):
-     return ""
+    return "Hello I am Alice!"
 
  #second person:
- def bob(action):
-     return ""
+def bob(action):
+    return "Hello I am Bob!"
 
 #third person:
- def dora(action):
-    return ""
+def dora(action):
+    return "Hello I am Dora!"
 
 #forth person:
- def chuck(action):
-    return ""
+def chuck(action):
+    return "Hello I am Chuck!"
